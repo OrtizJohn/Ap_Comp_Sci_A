a47d87a2bc55c5bc08c78ab94669b72702d0c097
OrtizJohn <johno2k@san.rr.com>
2018-02-06 10:41:22 -0800

    Project

---
+++ Unit2and3/src/Lab02e.java
@@ -0,0 +1,18 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - John Ortiz
+//Date - 2/5
+//Class -
+//Lab  -
+
+public class Lab02e
+{
+	public static void main( String[] args )
+   {
+		//add test cases
+		Circle c1 = new Circle();
+		c1.calculateArea(7.5);
+		c1.print();
+		
+		
+	}
+}
---
+++ Unit2and3/src/Lab02f.java
@@ -0,0 +1,22 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab02f
+{
+	public static void main( String[] args )
+   {
+		Line l1 = new Line(2,1,3,4);
+		l1.calculateSlope();
+		l1.print();
+		l1.setCoordinates(5, 10, 20, 30);
+		l1.calculateSlope();
+		l1.print();
+		
+		
+		
+		
+	}
+}
