30bd72b4dd0a29cd42106b8a9c678ae78771b34c
OrtizJohn <johno2k@san.rr.com>
2018-03-28 11:57:25 -0700

    unit 13

--- Unit 14/src/Maze.java
+++ Unit 14/src/Maze.java
@@ -1,3 +1,4 @@
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -12,69 +13,66 @@ import static java.lang.System.*;
 public class Maze
 {
    private int[][] maze;
-
+   private boolean exitFound = false;
 	public Maze()
 	{
-		maze= new int [][]{};
-
-
+		maze = new int [][]{};
+		
+		
 	}
 
 	public Maze(int size, String line)
 	{
 		maze = new int[size][size];
 		Scanner scan = new Scanner(line);
-		for(int i = 0; i < size; i++)
-		{
-			for(int j = 0; j < size; j++)
-			{
+		for (int i = 0; i < size; i++){
+			for (int j = 0; j < size; j++){
 				maze[i][j] = scan.nextInt();
 			}
 		}
-
 
 	}
 
 	public void hasExitPath(int r, int c)
 	{
-		if(r>=0 && c>=0 && r<maze.length && c<maze[r].length && maze[r][c] == 1){
-			maze[r][c]= 4;
-			if(maze[r-1][c]==1){
+		if (r >= 0 && c>= 0 && r<maze.length && c<maze[r].length && maze[r][c] == 1){
+			if(c == maze.length - 1){
+				exitFound = true;
+			}
+			else{
+				maze[r][c] = 4;
 				hasExitPath(r-1,c);
-			}
-			if(maze[r+1][c]==1){
 				hasExitPath(r+1,c);
-			}
-			if(maze[r][c-1]==1){
 				hasExitPath(r,c-1);
-			}
-			if(maze[r][c+1]==1){
 				hasExitPath(r,c+1);
 			}
 			
 		}
-		if(maze[r][c]==1 && c==maze[r].length){
-			System.out.println("exit found");
-		}
-		else{
-			System.out.println("exit not found");
-		}
-		
-
 		
 	}
 
 	public String toString()
 	{
-		String output="";
-		output += maze;
 		hasExitPath(0,0);
-		
-
-
-
-
-
+		String output="";
+		for(int i = 0; i < maze.length; i++){
+			for(int j = 0; j < maze.length; j++){
+				if(maze[i][j] == 4){
+					output += 1 + " ";
+				}
+				else{
+					output += 0 + " ";
+				}
+			}
+			output += "\n";
+		}
+		if(exitFound){
+			output += "exit found\n";
+		}
+		else{
+			output += "exit not found\n";
+		}
 		return output;
 	}
 }
+
